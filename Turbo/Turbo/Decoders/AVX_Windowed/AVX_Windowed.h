1c04b0bea3e085a96336699949e90ec5f697ca48
CaneRig <originalomail@gmail.com>
2025-01-22 02:04:40 +0300

    Refactor

--- Turbo/Turbo/Decoders/AVX_Windowed/AVX_Windowed.h
+++ Turbo/Turbo/Decoders/AVX_Windowed/AVX_Windowed.h
@@ -12,6 +12,21 @@ namespace turbo {
 
 	// AVX2 realisation of turbo decoder with Windowing optimization
 	class WindowedDecoderAVX {
+	public:
+		WindowedDecoderAVX() {
+
+		}
+
+		~WindowedDecoderAVX() {}
+
+		 /**
+		 *	@in		 - input array where negative values are probability of being zero, and positive of being one
+		 *	@iters	 - number of decoding iterations
+		 *	@full_out- decoder destination
+		 *	@return  - true if input values are correct, false overwize
+		 **/
+		bool Decode(const dec_arr_t& in, int iters, byte_arr_t& full_out);
+	private:
 		static const int _MAX_IN_BLOCK_SIZE = 6144 + 4;
 
 		using _avarr_t = std::array<avx::simd_t, _MAX_IN_BLOCK_SIZE / 2 + 1>;
@@ -29,20 +44,6 @@ namespace turbo {
 		avx::simd_t _init_alpha, _init_beta;
 
 		void _SISO();
-	public:
-		WindowedDecoderAVX() {
-
-		}
-
-		~WindowedDecoderAVX() {}
-
-		 /**
-		 *	@in		 - input array where negative values are probability of being zero, and positive of being one
-		 *	@iters	 - number of decoding iterations
-		 *	@full_out- decoder destination
-		 *	@return  - true if input values are correct, false overwize
-		 **/
-		bool Decode(const dec_arr_t& in, int iters, byte_arr_t& full_out);
 	};
 }
 #endif
